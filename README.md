42549d68074e5d7af7f01779d60a4a9ff9792e62
Alec Mouri <alec.mouri@gmail.com>
2014-05-30 19:53:53 -0700

    ImageJ dependencies

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-NeuroVidJ
+NeuroVizJ
 =========
+
+An image processing application for making it easy for researchers
+to perform cell tracking and data collection.
+
+This project is based off of NeuroViz and is developed in Java, using
+ImageJ as an image processing library.
---
+++ edu/uci/ics/graphics/neurovizj/src/segmentationtracking/Segmentator.java
@@ -0,0 +1,12 @@
+package edu.uci.ics.graphics.neurovizj.src.segmentationtracking;
+
+/**
+ * This class will perform segmentation on an image
+ * @author Alec
+ *
+ */
+public class Segmentator {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
